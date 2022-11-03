be6ad43431af5e99a53410236b1c6bed50f09d1e
Gareth Field <field.gareth@gmail.com>
2022-11-03 08:05:31 -0400

    seed and whoops forgot to filter, do wavs even work?

--- shuffle.js
+++ shuffle.js
@@ -1,13 +1,33 @@
-const path = require('path');
-const fs = require('fs');
-const os = require('os');
+import path from 'path';
+import fs from 'fs';
+import os from 'os';
+import { nanoid } from 'nanoid';
 
 const EXTNAME = '.mp3';
+const HOME = os.homedir();
+const SHUFFLE_FOLDER = `${HOME}/Music/mp3-player`;
+const RANDOM_NUMBER = 7;
 
-fs.readdir(`${os.homedir()}/Music`, (err, files) => {
+fs.readdir(SHUFFLE_FOLDER, (err, files) => {
 	if (err) {
 		console.error('Could not list the directory.', err);
 		process.exit(1);
 	}
-	console.log(files);
+
+	files.forEach((file, index) => {
+		// Make one pass and make the file complete
+		const fromPath = path.join(SHUFFLE_FOLDER, file);
+		const toPath = path.join(
+			SHUFFLE_FOLDER,
+			nanoid(RANDOM_NUMBER) + '-' + file
+		);
+
+		fs.rename(fromPath, toPath, function (error) {
+			if (error) {
+				console.error('File moving error.', error);
+			} else {
+				console.log("Moved file '%s' to '%s'.", fromPath, toPath);
+			}
+		});
+	});
 });
