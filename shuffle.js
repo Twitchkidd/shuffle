72c4a8bd4fb5104cc5c0202cc58edab3169d72ed
Gareth Field <field.gareth@gmail.com>
2022-11-04 07:34:54 -0400

    yayy

--- shuffle.js
+++ shuffle.js
@@ -3,9 +3,9 @@ import fs from 'fs';
 import os from 'os';
 import { nanoid } from 'nanoid';
 
-const EXTNAME = '.mp3';
 const HOME = os.homedir();
 const SHUFFLE_FOLDER = `${HOME}/Music/mp3-player`;
+const EXTNAME = '.mp3';
 const RANDOM_NUMBER = 7;
 
 fs.readdir(SHUFFLE_FOLDER, (err, files) => {
@@ -14,20 +14,40 @@ fs.readdir(SHUFFLE_FOLDER, (err, files) => {
 		process.exit(1);
 	}
 
-	files.forEach((file, index) => {
-		// Make one pass and make the file complete
-		const fromPath = path.join(SHUFFLE_FOLDER, file);
-		const toPath = path.join(
-			SHUFFLE_FOLDER,
-			nanoid(RANDOM_NUMBER) + '-' + file
-		);
+	const targetFiles = files.filter(
+		file =>
+			path.extname(path.join(SHUFFLE_FOLDER, file)).toLowerCase() === EXTNAME
+	);
 
-		fs.rename(fromPath, toPath, function (error) {
-			if (error) {
-				console.error('File moving error.', error);
-			} else {
-				console.log("Moved file '%s' to '%s'.", fromPath, toPath);
-			}
-		});
+	targetFiles.forEach((file, index) => {
+		if (file[RANDOM_NUMBER] !== '-') {
+			const fromPath = path.join(SHUFFLE_FOLDER, file);
+			const toPath = path.join(
+				SHUFFLE_FOLDER,
+				nanoid(RANDOM_NUMBER) + '-' + file
+			);
+
+			fs.rename(fromPath, toPath, function (error) {
+				if (error) {
+					console.error('File moving error.', error);
+				} else {
+					console.log("Moved file '%s' to '%s'.", fromPath, toPath);
+				}
+			});
+		} else {
+			const fromPath = path.join(SHUFFLE_FOLDER, file);
+			const toPath = path.join(
+				SHUFFLE_FOLDER,
+				nanoid(RANDOM_NUMBER) + file.slice(RANDOM_NUMBER)
+			);
+
+			fs.rename(fromPath, toPath, function (error) {
+				if (error) {
+					console.error('File moving error.', error);
+				} else {
+					console.log("Moved file '%s' to '%s'.", fromPath, toPath);
+				}
+			});
+		}
 	});
 });
